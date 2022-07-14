e0b1154903516ffa31b608f1c74bc28c18c0dee5
andreynikonov23 <76613379+andreynikonov23@users.noreply.github.com>
2022-07-14 21:09:17 +0300

    ConfigurationViaJava

---
+++ spring-mvc-test/src/main/java/nick/pack/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package nick.pack.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+	@GetMapping("/test")
+	public String test() {
+		return "test";
+	}
+}
